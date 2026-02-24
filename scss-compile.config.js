314f9287934b912ec851f95bcb55e02703aca0e4
LennyLuffeLs <LennyLuffeLs@users.noreply.github.com>
2026-02-24 15:26:41 +0000

    deploy: 2ba0cba93c6c86ee180554d65f39d36b948d4b8d

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Synthesis" /* The official name of your theme. */,
+        scss: "synth" /* The name of your base scss file. */,
+        repo: "synthesis-discord-theme" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
